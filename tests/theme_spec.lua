c5e196bdf2a2cb981d2815ca34f80d604f77298e
Lukas Alexandre <the.lukelex@gmail.com>
2026-09-21 16:05:23 +0200

    Test high contrast palette reload

--- tests/theme_spec.lua
+++ tests/theme_spec.lua
@@ -8,3 +8,7 @@ assert(vim.g.colors_name == "railscasts")
 assert(highlight("Normal").bg == 0x2B2B2B)
 assert(highlight("diffRemoved").fg == highlight("DiffDelete").fg)
 assert(require("lualine.themes.railscasts").normal.a.bg == "#87AF5F")
+
+vim.g.railscasts_high_contrast = true
+vim.cmd.colorscheme("railscasts")
+assert(require("railscasts.colors").dark_green == "#5FAF5F")
