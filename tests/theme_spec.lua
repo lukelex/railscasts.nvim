9ee155ef1eb2a3393e02d9213bf98cac54d90847
Lukas Alexandre <the.lukelex@gmail.com>
2026-09-21 16:22:17 +0200

    Add highlights for common UI plugins

--- tests/theme_spec.lua
+++ tests/theme_spec.lua
@@ -8,6 +8,9 @@ assert(vim.g.colors_name == "railscasts")
 assert(highlight("Normal").bg == 0x2B2B2B)
 assert(highlight("diffRemoved").fg == highlight("DiffDelete").fg)
 assert(require("lualine.themes.railscasts").normal.a.bg == "#87AF5F")
+assert(highlight("GitSignsDelete").fg == highlight("DiffDelete").fg)
+assert(highlight("BlinkCmpMenuSelection").bg == highlight("PmenuSel").bg)
+assert(highlight("IblScope").fg == 0xAF5F00)
 
 vim.g.railscasts_high_contrast = true
 vim.cmd.colorscheme("railscasts")
