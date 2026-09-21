931c679695dc4b24b637dcb13502ebe775745d81
Lukas Alexandre <the.lukelex@gmail.com>
2026-09-21 16:00:04 +0200

    Add headless theme smoke tests

---
+++ tests/theme_spec.lua
@@ -0,0 +1,10 @@
+vim.cmd.colorscheme("railscasts")
+
+local function highlight(name)
+  return vim.api.nvim_get_hl(0, { name = name, link = false })
+end
+
+assert(vim.g.colors_name == "railscasts")
+assert(highlight("Normal").bg == 0x2B2B2B)
+assert(highlight("diffRemoved").fg == highlight("DiffDelete").fg)
+assert(require("lualine.themes.railscasts").normal.a.bg == "#87AF5F")
